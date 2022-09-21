5e062dba1e1fe7a9610a2ffef2c8cf935cbf5a63
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-09-20 23:01:25 -0300

    iniciando meu perfil

--- labefood/src/pages/meu-perfil/MeuPerfil.js
+++ labefood/src/pages/meu-perfil/MeuPerfil.js
@@ -2,14 +2,42 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { goToEditarCadastro } from '../../rotas/Coordinator';
 import { goToEditarEndereco } from '../../rotas/Coordinator';
-
+import axios from 'axios';
+import { BASE_URL } from '../../constants/constants';
+import { useEffect, useState } from 'react';
 
 export const MeuPerfil = () => {
     const navigate = useNavigate()
 
+    const [informacoes, setInformacoes] = useState([]);
+
+    useEffect (() => {
+        listaInformacoes()
+        .then(infos => {
+            setInformacoes(infos);
+        })
+        .catch((e) => {
+            console.log(e)
+        })
+
+    }, [])
+
+    const listaInformacoes = async () => {
+        const {infos} = await axios.get(`${BASE_URL}profile`,
+        {
+            headers: {
+                Authorization: localStorage.getItem("token")                
+            }
+        }    
+           
+        );
+        return infos
+    }
+
     return (
         <div>
-            <>Meu Perfil</>
+            <h2>Meu Perfil</h2>
+            {informacoes.slice(0,20).map((informacao, i) => JSON.stringify(informacao)) }
             <button onClick={() => {goToEditarCadastro(navigate)}}> Editar Cadastro </button>
             <button onClick={() => {goToEditarEndereco(navigate)}}> Editar Endereço </button>
         </div>
@@ -17,4 +45,3 @@ export const MeuPerfil = () => {
 
 
 }
-
