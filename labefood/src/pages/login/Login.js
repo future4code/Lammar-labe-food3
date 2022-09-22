1ce9d86b3c0785e310e64d865ab184330aad29a4
Leandro Lima dos Santos <leandrolsadm@gmail.com>
2022-09-21 21:14:21 -0300

    fix and tests

--- labefood/src/pages/login/Login.js
+++ labefood/src/pages/login/Login.js
@@ -1,95 +1,89 @@
-import React, {useEffect, useState} from 'react'
-import { useNavigate } from 'react-router-dom';
-import { goToSignUp } from '../../rotas/Coordinator';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { goToSignUp } from "../../rotas/Coordinator";
 import axios from "axios";
-import { useForm } from '../../hooks/useForm/useForm';
-import TextField from '@mui/material/TextField';
-import Rappi4 from "../../img/Rappi4.svg"
-import { useRequestData } from '../../hooks/useRequestData/useRequestData';
-import { BASE_URL } from "../../constants/constants";
+import { useForm } from "../../hooks/useForm/useForm";
+import TextField from "@mui/material/TextField";
+import Rappi4 from "../../img/Rappi4.svg";
+import { SplashScreen } from "../../components/filters/splashScreen/SplashScreen";
 
 
 export const Login = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const [form, onChange, clear] = useForm({ email: "", password: "" });
 
-  const [isLoading, setIsLoading] = useRequestData(
-    `${BASE_URL}`
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+  });
+
+  const loginStart = (event) => {
+    event.preventDefault();
+
+    axios
+      .post(
+        "https://us-central1-missao-newton.cloudfunctions.net/rappi4A/login",
+        form
+      )
+      .then((response) => {
+        localStorage.setItem("token", response.data.token);
+        navigate("/feed");
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message));
+    clear();
+  };
+
+  return (
+    <div>
+     
+     {loading ? <SplashScreen />:
+   <div>
+      <img src={Rappi4} alt="logo-Rappi4" />
+
+      <h2>Login</h2>
+
+      <form onSubmit={loginStart}>
+        <TextField
+          name={"email"}
+          label={"E-mail"}
+          value={form.email}
+          onChange={onChange}
+          placeholder="E-mail"
+          type={"email"}
+          required
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <br />
+        <TextField
+          name={"password"}
+          label="Senha"
+          value={form.password}
+          onChange={onChange}
+          placeholder="Senha"
+          type="password"
+          required
+          pattern={"^.{6,}"}
+          title={"A senha deve ter no mínimo 6 caracteres"}
+          variant="outlined"
+          fullWidth
+          margin="normal"
+        />
+        <button type="submit">Entrar</button>{" "}
+        <button
+          onClick={() => {
+            goToSignUp(navigate);
+          }}
+        >
+          Cadastrar
+        </button>
+      </form>
+      </div>}
+    </div>
   );
-  const [loading, setLoading] = useState(undefined);
-
-
-useEffect(()=>{ 
-  // setLoading(true) 
-   setTimeout(() => {
-    setLoading(false)
-  }, 5000);
-})
-
-
-    const loginStart = (event) => {
-        event.preventDefault();
-        axios
-          .post(
-            "https://us-central1-missao-newton.cloudfunctions.net/rappi4A/login",
-            form
-          )
-          .then((response) => {
-            localStorage.setItem("token", response.data.token);
-            navigate("/feed");
-          })
-          .catch((error) =>  alert(error.message));
-        clear();
-      };
-
-
-    return (
-        <div>
-          <div>
-          {setIsLoading &&  "...Carregando!!! ...."}
-          </div>
-
-            <img src={Rappi4} alt="logo-Rappi4"/>
-            <h2>Login</h2>
-           
-            <form onSubmit={loginStart}>
-           
-            <TextField
-            name={"email"}
-            label={"E-mail"}
-            value={form.email}
-            onChange={onChange}
-            placeholder="E-mail"
-            type={"email"}
-            required
-            variant="outlined"
-            fullWidth
-            margin="normal"
-        
-          />
-
-            <br />
-            <TextField
-            name={"password"}
-            label="Senha"
-            value={form.password}
-            onChange={onChange}
-            placeholder="Senha"
-            type="password"
-            required
-            pattern={"^.{6,}"}
-            title={"A senha deve ter no mínimo 6 caracteres"}
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-            <button type="submit">Entrar</button>{" "}
-            <button onClick={() => {goToSignUp(navigate)}}>Cadastrar</button>
-            </form>
-        </div>
-
-
-  );
-
-
-}
+};
