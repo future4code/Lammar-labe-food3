9a282c50c8ae19a259d4b0097f50c6b4e6a3bd6d
unknown <vmotta95@gmail.com>
2022-09-21 21:44:35 -0300

    pagina busca em andamento, arrumando logica

--- labefood/src/pages/busca/Busca.js
+++ labefood/src/pages/busca/Busca.js
@@ -2,28 +2,31 @@ import React from 'react'
 import axios from 'axios'
 import { useRequestData } from '../../hooks/useRequestData/useRequestData'
 import { BASE_URL } from '../../constants/constants'
-import { Filters } from '../../components/filters/Filters'
+import { FiltersForSeach } from '../../components/filters/Filters'
 import { useState } from 'react'
 
-export const Busca = ({ handleClick }) => {
-const [filterByName, setFilterByName] = useState("")
+export const Busca = () => {
+const [inputFilterByName, setInputFilterByName] = useState("")
 
 const [data] = useRequestData(
-    `${BASE_URL}/restaurants}`);
+    `${BASE_URL}restaurants}`);
+    console.log(data)
+
+    // const filterRestaurants =
+    // data &&
+    // data.filter((item) => {
+    //   if (inputFilterByName !== "") {
+    //     return item.name.toLowerCase().includes(inputFilterByName.toLowerCase());
+    //   // } else if (category !== "") {
+    //   //   return category === item.category;
+    //   // } else {
+    //     return item;
+    //   }
+    // });
 
     return (
-            <div>
-            <Filters
-            filterByName={filterByName}
-            setFilterByName={setFilterByName}/>
-            <div>
-            {data.filter(produto => {
-        return produto.restaurants.name.toLowerCase().includes(filterByName)
-      }).map(produto => {
-        return <div key={produto.id}
-         name={produto.restaurants.name}
-         handleClick={handleClick}/>
-})}     </div>
+        <div>
+           
         </div>
     )
 
