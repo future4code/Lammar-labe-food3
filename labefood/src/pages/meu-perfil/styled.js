73e06126ddca1756d9a1387f8447f2a648a53a76
Julia Marques Rodrigues de Lima <juliamarques00@hotmail.com>
2022-09-27 16:20:27 -0300

    styled meu perfil

--- labefood/src/pages/meu-perfil/styled.js
+++ labefood/src/pages/meu-perfil/styled.js
@@ -1,50 +1,76 @@
 import styled from "styled-components";
 
 export const Tela  = styled.div`
-    width: 360px;
-    height: 640px;
+    display: flex;
+    flex-direction: column;
+    width: 400px;
+    height: 600px;
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    margin: auto;
 
 p {
-    width: 328px;
-    height: 18px;
-    margin: 16px 16px 8px;
-    font-family: Roboto;
-    font-size: 16px;
-    font-weight: normal;
-    font-stretch: normal;
-    font-style: normal;
-    line-height: normal;
-    letter-spacing: -0.39px;
-    color: #000;
-}
-
-img {
-    width: 24px;
-    height: 24px;
-    margin: 16px 16px 2px 320px;
-    object-fit: contain;
-}
-
-`
-export const Rectangle  = styled.div`
-  width: 360px;
-  height: 76px;
-  margin: 16px 0;
-  padding: 16px;
-  background-color: #eee;
-`
-export const Endereco  = styled.div`
-  width: 328px;
-  height: 18px;
-  margin: 0 0 8px;
+   
   font-family: Roboto;
   font-size: 16px;
   font-weight: normal;
   font-stretch: normal;
   font-style: normal;
   line-height: normal;
+  color: #000;
   letter-spacing: -0.39px;
+  line-height: 180%;
+    
+}
+
+img {
+    width: 24px;
+    height: 24px;
+    
+}
+
+h2{
+  margin: 10px;
+  margin-top: 30px;
+  margin-bottom:10px;
+  text-align: center;
+  border-bottom: 1px solid rgba(0, 0, 0, 0.25);
+  padding: 10px;
+}
+
+h3{
+  color: grey;
+}
+
+h4 {
+  margin: 10px;
+  border-bottom: 1px solid black;
+  padding-bottom: 5px;
+  
+
+}
+
+
+`
+export const Rectangle  = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 10px;
+  padding: 10px;
+  background-color: #eee;
+`
+export const Endereco  = styled.div`
+ 
   color: var(--greyish);
+`
+
+export const Informacoes = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 10px;
+
+
+
+
 `
 
 
